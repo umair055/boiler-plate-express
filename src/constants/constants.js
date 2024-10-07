1e68a1e42ad39bd3b94265aae4b7508854948f6d
Umair Tariq <umair.tariq@focusteck.com>
2024-10-07 11:06:22 +0500

    Invites modules fully implemented

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,12 +1,14 @@
 const CONSTANTS = {
-  PUBLIC_ROUTES: ['/auth/signup', '/auth/login'],
+  PUBLIC_ROUTES: ['/auth/signup', '/auth/login', '/invites/accept-invite', '/resend-invite/:email', '/verify-code/:code'],
   ROLES: {
     SUPER_ADMIN: 'Super Admin',
     ADMIN: 'Admin',
     VENDOR_MANAGER: 'Vendor Manager',
     PROJECT_MANAGER: 'Project Manager',
     MEMBER: 'Member',
-  }
+  },
+  InviteStatus: { PENDING: 'pending', APPROVED: 'approved' },
+  UserStatus: { ACTIVE: 'active', PENDING: 'pending', DELETED: 'deleted' }
 }
 
 module.exports = CONSTANTS
